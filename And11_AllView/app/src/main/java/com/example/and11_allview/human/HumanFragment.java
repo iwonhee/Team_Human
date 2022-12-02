5823adcfa98d8da4e2848bb64fc852af9f1d4813
wonhee <asdkcone13@gmail.com>
2022-12-02 10:41:49 +0900

    first upload
    작업 이상한곳에서 하고 있었음 ㄷㄷ

--- And11_AllView/app/src/main/java/com/example/and11_allview/human/HumanFragment.java
+++ And11_AllView/app/src/main/java/com/example/and11_allview/human/HumanFragment.java
@@ -3,12 +3,16 @@ package com.example.and11_allview.human;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.and11_allview.R;
+
+import java.util.ArrayList;
 
 public class HumanFragment extends Fragment {
 
@@ -17,7 +21,18 @@ public class HumanFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_human, container, false);
 
+        RecyclerView recy_human = v.findViewById(R.id.recv_human);
 
+        ArrayList<HumanDTO> list = new ArrayList<>();
+
+        list.add(new HumanDTO());
+
+        recy_human.setAdapter(new HumanAdapter(inflater, list));
+        // 리사이클러뷰 매니저 설정
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(
+                getContext(), RecyclerView.VERTICAL, false
+        );
+        recy_human.setLayoutManager(manager);
 
         return v;
     }
